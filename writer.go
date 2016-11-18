20c95712a0271c81b8134677d40b48d3d7f566cb
Bogdan Guranda <bogdan.guranda@ft.com>
2016-11-18 15:31:09 +0200

    Fixed file not being uploaded to S3.

--- writer.go
+++ writer.go
@@ -8,7 +8,7 @@ import (
 )
 
 type Writer interface {
-	Write(src string, resName string) error
+	Write(src string, localFileName string, s3FileName string) error
 }
 
 type S3Writer struct {
@@ -20,23 +20,23 @@ func NewWriter(config s3Config) (Writer, error) {
 	return &S3Writer{s3Client: s3}, err
 }
 
-func (s3w *S3Writer) Write(src string, resName string) error {
-	name := s3w.gets3ResName(resName)
-	p := path.Join(src, resName)
-	n, err := s3w.s3Client.PutObject(name, p)
+func (s3w *S3Writer) Write(src string, localFileName string, s3FileName string) error {
+	s3ResFilePath := s3w.getS3ResFilePath(s3FileName)
+	p := path.Join(src, localFileName)
+	n, err := s3w.s3Client.PutObject(s3ResFilePath, p)
 	if err != nil {
 		return err
 	}
-	log.Infof("Uploaded file [%s] of size [%d] successfully", name, n)
+	log.Infof("Uploaded file [%s] of size [%d] successfully", s3ResFilePath, n)
 	return nil
 }
 
-func (s3w *S3Writer) gets3ResName(res string) string {
-	if res == "" {
-		return res
+func (s3w *S3Writer) getS3ResFilePath(s3FileName string) string {
+	if s3FileName == "" {
+		return s3FileName
 	}
 
-	resFilePath := "financial-instruments/" + time.Now().Format("2006-01-02") + "/" + res
+	resFilePath := "financial-instruments/" + time.Now().Format("2006-01-02") + "/" + s3FileName
 
 	return resFilePath
 }
